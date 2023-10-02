a1a111169ca3dce3788e5c0556a8672ea49ed4a7
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2023-10-02 20:16:37 +0530

    Useless

--- CAIbot/__init__.py
+++ CAIbot/__init__.py
@@ -1,8 +1,6 @@
 import os
-from dotenv import load_dotenv
 from pyrogram import Client
 
-load_dotenv()
 api_id = os.environ.get("API_ID")
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("TOKEN")
